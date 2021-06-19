2dd2fdbc0d5049c643abe589addf86ee42963ee9
Sadia131 <55445830+Sadia131@users.noreply.github.com>
2021-06-19 14:49:53 +0600

    Add files via upload
    Lab_Task_3

---
+++ Lab3/Picture.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+		<fieldset>
+			    <legend><b>PROFILE PICTURE</b></legend>
+   				 <img src="uploads/account-avatar-contact-picture-icon.png" alt="Image Icon"><br>
+    			<input type="file" name="fileToUpload" id="fileToUpload"><br>
+   				 <input type="submit" value="Submit" name="submit">
+		</fieldset>
+	  
+	</form>
+</body>
+</html>
